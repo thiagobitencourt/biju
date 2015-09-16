4864482fcb5afda69dc56ccc9f717e260a45cece
Thiago R. M. Bitencourt <thiago.mbitencourt@gmail.com>
2015-09-15 23:28:43 -0300

    Teste 10. Relacioando com Tarefa #1

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 //Starting point
 
-//Test of integration between bitbucket and redmine. New test. Test 9
+//Test of integration between bitbucket and redmine. New test. Test 10, relacionando com tarefa
