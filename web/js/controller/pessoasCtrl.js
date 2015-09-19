7e18d6370c6e7bdd7eb416358fb04786bfd6aec9
Mohamad Abu Ali <arabian@brasnet.org>
2015-09-19 00:22:43 -0300

    tabela de pessoas tarefa #20 @0.5

--- web/js/controller/pessoasCtrl.js
+++ web/js/controller/pessoasCtrl.js
@@ -1,7 +1,43 @@
 var app = angular.module('bijuApp');
 
-app.controller('pessoasCtrl', function($scope){
+app.controller('pessoasCtrl', function($scope, Restangular){
 
-	$scope.pessoa = {nome: "mohamad", idade: 32};
+	//@TODO corregir o service RESTANGULAR
+
+	$scope.pessoasScopeProvider = {
+		details: function(row){
+			console.log(row.entity);
+		}
+	};
+
+	$scope.pessoasGridOptions = {
+	    paginationPageSizes: [5, 10, 25, 50, 100],
+    	paginationPageSize: 5,
+	    multiSelect: false,
+	    enableRowSelection: true, 
+	    enableSelectAll: false,
+	    enableRowHeaderSelection: false,
+	    selectionRowHeaderWidth: 35,
+	    onRegisterApi: function(gridApi){ 
+	      $scope.gridApi = gridApi;
+	    },
+	    appScopeProvider: $scope.pessoasScopeProvider,
+	    rowTemplate: 'view/template-dblclick.html'
+	 };
+
+	$scope.pessoasGridOptions.columnDefs = [
+	      { name: 'nome', displayName: 'Nome'},
+	      { name: 'telefoneFixo', displayName: 'Fixo'},
+	      { name: 'telefoneCelular', displayName: 'Celular'},
+	      { name: 'email', displayName: 'E-mail'},
+	      { name: 'pessoaReferencia', displayName: 'P. Ref.'},
+	      { name: 'status', displayName: 'Status'}
+	];
+
+	var _loadPessoas = function(){
+		$scope.pessoasGridOptions.data = Restangular.all('pessoa').getList().$object;
+	};
+
+	_loadPessoas();
 
 });
