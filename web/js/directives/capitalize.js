ad4be8efaf5ba66871de53b52b458974e8772c58
Mohamad Abu Ali <arabian@brasnet.org>
2015-09-19 21:31:01 -0300

    adicionado validacao em detalhes e form tarefa #21 @0.5h

---
+++ web/js/directives/capitalize.js
@@ -0,0 +1,19 @@
+var app = angular.module('bijuApp');
+app.directive('capitalize', function() {
+   return {
+     require: 'ngModel',
+     link: function(scope, element, attrs, modelCtrl) {
+        var capitalize = function(inputValue) {
+           if(inputValue == undefined) inputValue = '';
+           var capitalized = inputValue.toUpperCase();
+           if(capitalized !== inputValue) {
+              modelCtrl.$setViewValue(capitalized);
+              modelCtrl.$render();
+            }         
+            return capitalized;
+         }
+         modelCtrl.$parsers.push(capitalize);
+         capitalize(scope[attrs.ngModel]);  // capitalize initial value
+     }
+   };
+});
